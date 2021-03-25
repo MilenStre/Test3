e785d362f98e84ec0661bd37aa5a038f0e12fcdd
Shahar1313 <Shahar@DESKTOP-5SU5H3B>
2021-03-25 16:07:08 +0200

    Last

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,41 +1,91 @@
+import java.util.ArrayList;
+
+/**
+ * saves the shared data between threads 
+ * @author Shahar 
+ *
+ */
 public class SharedData 
 {
-	private int [] array;
+/**
+ * array of numbers in SharedData7 
+ */
+	private ArrayList<Integer> array;
+	/**
+	 * boolean array in SharedData
+	 */
 	private boolean [] winArray;
+	/**
+	 * flag for the status of the threads (has found the numbers or not )
+	 */
 	private boolean flag;
+	/**
+	 * the number that we need to reach
+	 */
 	private final int b;
 	
-	public SharedData(int[] array, int b) {
+	/**
+	 * Constructor for SharedData
+	 * @param array   saves the array with numbers
+	 * @param b   saves the number that we want to reach
+	 */
+	
+	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
 
+	/**s
+	 * 
+	 * @return returns a boolean array with the indexes of the numbers that have been chosen for the final answer
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
+	//
 
+	/**
+	 * 
+	 * @param winArray saves the indexes of the chosen numbers in a boolean array
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
+	/**
+	 * 
+	 * @return returns the array with the numbers
+	 */
+	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/**
+	 * 
+	 * @return returns the number "b" that we want to reach
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * 
+	 * @return returns the flag of sharedsData that indicates the status of a thread(finish or not)
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**
+	 * 
+	 * @param flag indicates if we found the answer and finished
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
--- ThreadCheckArray/src/ThreadCheckArray.java
+++ ThreadCheckArray/src/ThreadCheckArray.java
@@ -1,11 +1,36 @@
+import java.util.ArrayList;
+/**
+ * in this class the threads looking for the numbers that build the required sum 
+ * @author Shahar
+ *
+ */
 public class ThreadCheckArray implements Runnable 
 {
+	/**ss
+	 * flag for the status of the threads(has found the numbers or not )
+	 */
 	private boolean flag;
+	/**
+	 * boolean array in SharedData
+	 */
 	private boolean [] winArray;
+	/**
+	 * holds the shared data(arrays,b,flag)
+	 */
 	SharedData sd;
-	int[] array;
+	/**
+	 * array of numbers in SharedData 
+	 */
+	private ArrayList<Integer> array;
+	/**
+	 * the number that we need to reach
+	 */
 	int b;
-	
+	//
+	/**s
+	 * Constructor of ThreadCheckArray
+	 * @param sd   holds the shared data
+	 */
 	public ThreadCheckArray(SharedData sd) 
 	{
 		this.sd = sd;	
@@ -14,9 +39,14 @@ public class ThreadCheckArray implements Runnable
 			array = sd.getArray();
 			b = sd.getB();
 		}		
-		winArray = new boolean[array.length];
+		winArray = new boolean[array.size()];
 	}
 	
+	/**
+	 * recursive function that checks if the answer was found and continue if not
+	 * @param n  represent array size -1
+	 * @param b  represent the sum that we need to reach
+	 */
 	void rec(int n, int b)
 	{
 		synchronized (sd) 
@@ -26,7 +56,7 @@ public class ThreadCheckArray implements Runnable
 		}	
 		if (n == 1)
 		{
-			if(b == 0 || b == array[n-1])
+			if(b == 0 || b == array.get(n-1))
 			{
 				flag = true;
 				synchronized (sd) 
@@ -34,12 +64,12 @@ public class ThreadCheckArray implements Runnable
 					sd.setFlag(true);
 				}			
 			}
-			if (b == array[n-1])
+			if (b == array.get(n-1))
 				winArray[n-1] = true;
 			return;
 		}
 		
-		rec(n-1, b - array[n-1]);
+		rec(n-1, b - array.get(n-1));
 		if (flag)
 			winArray[n-1] = true;
 		synchronized (sd) 
@@ -50,14 +80,17 @@ public class ThreadCheckArray implements Runnable
 		rec(n-1, b);
 	}
 
+	/**
+	 * every thread looking for the numbers in the array that sums up the number "b"
+	 */
 	public void run() {
-		if (array.length != 1)
+		if (array.size() != 1)
 			if (Thread.currentThread().getName().equals("thread1"))
-				rec(array.length-1, b - array[array.length - 1]);
+				rec(array.size()-1, b - array.get(array.size() - 1));
 			else 
-				rec(array.length-1, b);
-		if (array.length == 1)
-			if (b == array[0] && !flag)
+				rec(array.size()-1, b);
+		if (array.size() == 1)
+			if (b == array.get(0) && !flag)
 			{
 				winArray[0] = true;
 				flag = true;
@@ -69,7 +102,7 @@ public class ThreadCheckArray implements Runnable
 		if (flag)
 		{
 			if (Thread.currentThread().getName().equals("thread1"))
-				winArray[array.length - 1] = true;
+				winArray[array.size() - 1] = true;
 			synchronized (sd) 
 			{
 				sd.setWinArray(winArray);
